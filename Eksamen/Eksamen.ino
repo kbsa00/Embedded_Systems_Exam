33443d7ffa777e73739c9eb4067d5799feef219a
Khalid B.S <khalid_bs@hotmail.com>
2018-04-11 21:12:45 +0200

    Buttons implemented

--- Eksamen/Eksamen.ino
+++ Eksamen/Eksamen.ino
@@ -1,3 +1,4 @@
+
 // This #include statement was automatically added by the Particle IDE.
 #include <HC-SR04.h>
 
@@ -7,10 +8,11 @@
 #define DHTTYPE DHT11
 #define DHTPIN D0
 #define TRIGGER A0
+
 #define ECHO A1
 #define buzzerOne D2
-#define buzzerTwo D3 
-
+#define pauseSystemButton  D3
+#define turnOnOffSystemButton D4
 
 
 
@@ -21,27 +23,62 @@ HC_SR04 moveSensor = HC_SR04(TRIGGER, ECHO);
 
 unsigned long thisTime;
 unsigned long lastTime;
+bool isSystem = true; 
+
+
+int lastPausebuttonPressed = 0;
+int pauseButtonState = 0;
+int turnOnOfSystemState = 0; 
+int lastTurnOfOnSystemPressed = 0; 
 
 
 void setup() {
     dht.begin();
     moveSensor.init();
     lastTime  = millis();
+    pinMode(pauseSystemButton, INPUT); 
+    pinMode(turnOnOffSystemButton, INPUT); 
+    
+    
 }
 
 void loop() {
     
-    thisTime = millis();
+    turnOnOfSystemState = digitalRead(turnOnOffSystemButton);
     
-    if(thisTime - lastTime > 300000)
-    {
+    if(isSystem){
+        HomeWatcherSystem(); 
+    }
+
+    if(turnOnOfSystemState == HIGH && lastTurnOfOnSystemPressed == LOW){
+        Particle.publish("BTN", "PRESSED"); 
+        //Turning on and off the System based on it's current state. 
+        if(isSystem != true){
+            isSystem = true;
+        }else{
+            isSystem = false; 
+        }
+        
+    }
+    
+    lastTurnOfOnSystemPressed = turnOnOfSystemState;
+    
+  
+}
+
+
+void HomeWatcherSystem(){
+    
+     thisTime = millis();
+    
+    pauseButtonState = digitalRead(pauseSystemButton); 
+    
+    if(thisTime - lastTime > 100000){
         String temp = String(dht.getTempCelcius());
         Particle.publish("Notifier", temp);
     }
     
-    
-     if(thisTime - lastTime > 2000)
-    {
+    if(thisTime - lastTime > 2000){
         float cm[10];
         float avgCm; 
         
@@ -55,22 +92,35 @@ void loop() {
         
         if(avgCm < 20)
         {
-              Particle.publish("Notifier", "SOMEONE IS HERE");
-              soundTheAlarm();  
-           // notifySlackMessageBoard("BURGLER ALERT", "SOMEONE IS IN YOUR APARTMENT!"); 
-            //TODO start the alarms!! 
+          Particle.publish("Notifier", "Home-Watcher picked up on movement in your apartment!!");
+          //soundTheAlarm();  
         }
         
         lastTime = millis();
     }
     
-  
+    if (pauseButtonState == HIGH && lastPausebuttonPressed == LOW ){
+        Particle.publish("AlarmNotifier", "The alarm was now turned off for 60 seconds! HURRY UP leaving the Student complex");
+       // PauseTheHomeWatcher();   
+    }
+    
+    lastPausebuttonPressed = pauseButtonState; 
 }
 
 
 void soundTheAlarm(){
-    
     tone(buzzerOne, 2000, 5000);
-    tone(buzzerTwo, 6000, 5000);
+}
+
+
+void PauseTheHomeWatcher(){
     
+    int index = 0;
+    
+    while(index <= 30){
+        
+        tone(buzzerOne, 2000, 500); 
+        delay(1000);
+        index++; 
+    }
 }
